f8e3058510013fd0a10be7a5710c3cee3cee4ca4
Rmx1921 <koreanplay14340@gmail.com>
2026-03-25 14:15:26 +0000

    421

--- drivers/clk/qcom/gpucc-sdm845.c
+++ drivers/clk/qcom/gpucc-sdm845.c
@@ -53,7 +53,7 @@ static int vdd_gx_corner[] = {
 	RPMH_REGULATOR_LEVEL_NOM_L1,		/* VDD_GX_NOMINAL_L1 */
 	RPMH_REGULATOR_LEVEL_TURBO,		/* VDD_GX_HIGH */
 	RPMH_REGULATOR_LEVEL_TURBO_L1,		/* VDD_GX_HIGH_L1 */
-	424,					/* VDD_GX_MAX */
+	421,					/* VDD_GX_MAX */
 };
 
 static DEFINE_VDD_REGULATORS(vdd_cx, VDD_CX_NUM, 1, vdd_corner);
@@ -294,7 +294,6 @@ static const struct freq_tbl ftbl_gpu_cc_gx_gfx3d_clk_src_sdm670[] = {
 	F(520000000, P_CRC_DIV,  1, 0, 0),
 	F(565000000, P_CRC_DIV,  1, 0, 0),
 	F(616000000, P_CRC_DIV,  1, 0, 0),
-	F(640000000, P_CRC_DIV,  1, 0, 0),
 	F(675000000, P_CRC_DIV,  1, 0, 0),
 	{ }
 };
@@ -657,7 +656,7 @@ static void gpu_cc_gfx_sdm845_fixup_sdm670(void)
 		520000000;
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH] = 616000000;
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH_L1] = 616000000;
-	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_MAX] = 640000000;
+	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_MAX] = 675000000;
 }
 
 static int gpu_cc_gfx_sdm845_fixup(struct platform_device *pdev)
